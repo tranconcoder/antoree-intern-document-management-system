6917c66a285fcaa3d5c8ecad9a316141a247b77b
tvconss <tranconcoder@gmail.com>
2025-08-25 13:49:55 +0700

    build: ErrorResponse class

--- nodejs/src/api/core/error.core.ts
+++ nodejs/src/api/core/error.core.ts
@@ -1,3 +1,29 @@
+import type { ErrorResponseItem } from "@/types/error";
+
 export default class ErrorResponse {
-  public constructor() {}
+  private errorResponseItem: ErrorResponseItem;
+  private detail?: string;
+
+  public constructor(errorResponseItem: ErrorResponseItem, detail?: string) {
+    this.errorResponseItem = errorResponseItem;
+    this.detail = detail;
+  }
+
+  //   Get error message handler
+  public get errorMessage(): string {
+    const now = new Date();
+
+    return `[${now.toISOString()}] [${this.errorResponseItem[0]}] ${
+      this.errorResponseItem[1]
+    } - ${this.errorResponseItem[2]}: ${this.detail || "..."}`;
+  }
+
+  public getError() {
+    return {
+      errorHttpCode: this.errorResponseItem[0],
+      errorName: this.errorResponseItem[1],
+      errorHttpReasonPhrase: this.errorResponseItem[2],
+      errorDetail: this.detail || "...",
+    };
+  }
 }
