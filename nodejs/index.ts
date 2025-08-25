deba19af5de0762a07d678cf7d5461e77adcb525
tvconss <tranconcoder@gmail.com>
2025-08-25 10:07:33 +0700

    install: type for morgan compression helmet

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,5 +1,12 @@
+import { morgan } from 'morgan';
 import express from "express";
-import 
+import helmet from 'helmet';
+import compression
+
+// Middleware
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(helmet())
+app.use(compression())
