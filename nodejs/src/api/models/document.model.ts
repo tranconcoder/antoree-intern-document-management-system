ca4bbd83c643dc67ba86fa859ee61dfc5e0bcbd0
tvconss <tranconcoder@gmail.com>
2025-08-26 11:57:41 +0700

    install: multer package

--- nodejs/src/api/models/document.model.ts
+++ nodejs/src/api/models/document.model.ts
@@ -8,8 +8,11 @@ import { model, Schema } from "mongoose";
 
 const documentSchema = new Schema(
   {
+    // Metadata
     title: { type: String, required: true, min: 10, max: 200 },
     description: { type: String, required: true, min: 20, max: 1000 },
+
+    // File information
     fileContentType: {
       type: String,
       required: true,
@@ -21,6 +24,11 @@ const documentSchema = new Schema(
       min: 1024 * 5, // 5 KB
       max: 1024 * 1024 * 10, // 5 MB
     },
+    fileSize: { type: Number, required: true },
+    fileName: { type: String, required: true, min: 3, max: 100 },
+
+    isPremium: { type: Boolean, default: false },
+    isPublic: { type: Boolean, default: false },
   },
   {
     timestamps: TIME_STAMPS,
