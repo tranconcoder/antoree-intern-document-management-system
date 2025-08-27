a9118d9e8cd0aa65a4eb212b7c5f97c091a8ce3d
tvconss <tranconcoder@gmail.com>
2025-08-27 22:03:52 +0700

    remove: sound on send lead success

--- nextjs/src/app/(home)/components/LeadComponent.tsx
+++ nextjs/src/app/(home)/components/LeadComponent.tsx
@@ -137,34 +137,6 @@ export default function LeadComponent() {
           navigator.vibrate([200, 100, 200]);
         }
 
-        // Play success sound (optional - using Web Audio API)
-        try {
-          const audioContext = new (window.AudioContext ||
-            (window as any).webkitAudioContext)();
-          const oscillator = audioContext.createOscillator();
-          const gainNode = audioContext.createGain();
-
-          oscillator.connect(gainNode);
-          gainNode.connect(audioContext.destination);
-
-          oscillator.frequency.setValueAtTime(800, audioContext.currentTime);
-          oscillator.frequency.setValueAtTime(
-            1000,
-            audioContext.currentTime + 0.1
-          );
-
-          gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-          gainNode.gain.exponentialRampToValueAtTime(
-            0.01,
-            audioContext.currentTime + 0.5
-          );
-
-          oscillator.start(audioContext.currentTime);
-          oscillator.stop(audioContext.currentTime + 0.5);
-        } catch (e) {
-          // Silence any audio errors
-        }
-
         // Auto hide notification after 6 seconds
         setTimeout(() => {
           setShowNotification(false);
