d60bab0819259f4b5afc6aa0d6844c10ed2ea597
tvconss <tranconcoder@gmail.com>
2025-08-30 23:09:16 +0700

    backup

--- nextjs/src/configs/axios.config.ts
+++ nextjs/src/configs/axios.config.ts
@@ -3,7 +3,7 @@ const getBaseURL = () => {
   const isDev = process.env.NODE_ENV !== "production";
   const useHTTPS = process.env.NEXT_PUBLIC_USE_HTTPS === "true" || false;
 
-  return "https://ec2-16-176-232-14.ap-southeast-2.compute.amazonaws.com:4000";
+  return "http://ec2-16-176-232-14.ap-southeast-2.compute.amazonaws.com:4000";
 };
 
 export const AXIOS_URL = getBaseURL();
